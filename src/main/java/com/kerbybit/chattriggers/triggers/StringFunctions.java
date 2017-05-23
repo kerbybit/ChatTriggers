6bbcd09a21311c9ddcf3d7517b0d54d3ed01d84c
FalseHonesty <skipboman0@gmail.com>
2017-05-23 15:58:07 -0400

    Actually imported Regex classes

--- src/main/java/com/kerbybit/chattriggers/triggers/StringFunctions.java
+++ src/main/java/com/kerbybit/chattriggers/triggers/StringFunctions.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.kerbybit.chattriggers.triggers.StringHandler.stringFunctions;
 import static java.lang.Math.abs;
